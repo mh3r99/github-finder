769710c1f28bb6dbfdb936724f48af1356520ca6
Mher Hovhannisyan <mh3r99@gmail.com>
2022-04-30 23:09:01 +0400

    github context

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,34 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
+import GithubContext from "../../context/github/GithubContext";
 import Spinner from "../Spinner";
+import UserItem from "./UserItem";
 
 function UserResults() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
-    console.log(users);
   }, []);
-
-  const fetchUsers = async () => {
-    const res = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await res.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (loading) return <Spinner />;
 
   return (
     <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
       {users.map((user) => (
-        <h3 key={user.id}>{user.login}</h3>
+        <UserItem key={user.id} user={user} />
       ))}
     </div>
   );
