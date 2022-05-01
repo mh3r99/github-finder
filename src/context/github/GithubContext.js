e59e76aa03316d394b94fad79dd7bd077523fc6d
Mher Hovhannisyan <mh3r99@gmail.com>
2022-05-01 19:18:05 +0400

    change token

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -4,7 +4,7 @@ import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({ children }) => {
   const initialState = {
@@ -25,7 +25,7 @@ export const GithubProvider = ({ children }) => {
 
     const res = await fetch(`${GITHUB_URL}/search/users?${params}`, {
       headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
+        // Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
@@ -43,7 +43,7 @@ export const GithubProvider = ({ children }) => {
 
     const res = await fetch(`${GITHUB_URL}/users/${login}`, {
       headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
+        // Authorization: `token ${GITHUB_TOKEN}`,
       },
     });
 
